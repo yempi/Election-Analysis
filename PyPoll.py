6379d5cdeec20395d5e0283287d22dbf43ff6716
Yesenia Perez Ibarra <83614893+yempi@users.noreply.github.com>
2022-01-22 21:28:07 -0600

    Add files via upload

---
+++ PyPoll.py
@@ -0,0 +1,45 @@
+# Data we need to retrieve
+# 1. Total number of votes cast
+# 2. Complete list of candidates who received votes
+# 3. Percentage of votes each candidate won
+# 4. Total number of votes each candidate won
+# 5. Winner of the election based on popular vote
+
+# Add dependencies
+import csv
+import os
+
+#Assign a variable for the  file to load and the path
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Initialize a total vote counter
+total_votes = 0
+
+# Candidate Options
+candidate_options = []
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+# Print the candidate list.
+print(candidate_options)
